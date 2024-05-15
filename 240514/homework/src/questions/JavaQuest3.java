de981b32aace07c1123e5a5fc4b99642103ff456
wingng <howing300713@gmail.com>
2024-05-16 03:09:16 +0800

    update

--- 240514/homework/src/questions/JavaQuest3.java
+++ 240514/homework/src/questions/JavaQuest3.java
@@ -12,7 +12,7 @@ public class JavaQuest3 {
    * 
    * Input a month number: 12
    * Input a year: 2014
-   * December 2014 has 31 days
+   * December 2014 has 31 days          
    */
   // Program toExercise3Ansumber of days that the month of a year has.
 
@@ -33,7 +33,8 @@ public class JavaQuest3 {
     switch (month) {
       case 1:
         MonthOfName = "January";
-        // Code here ...
+        number_Of_DaysInMonth = 31;
+        break;
       case 2:
         MonthOfName = "February";
         // The February has 29 days: (Every 4 years and the year cannot divded by 100)
@@ -41,6 +42,49 @@ public class JavaQuest3 {
         // i.e. 2200 Feb -> 28 days, 2400 Feb -> 29 days, 2016 Feb -> 29 days
         // otherwise the February should have 28 days only
         // how about other months, December?
+        break;
+        case 3:
+        MonthOfName = "March";
+        number_Of_DaysInMonth = 31;
+        break;
+        case 4:
+        MonthOfName = "April";
+        number_Of_DaysInMonth = 30;
+        break;
+        case 5:
+        MonthOfName = "May";
+        number_Of_DaysInMonth = 31;
+        break;
+        case 6:
+        MonthOfName = "Jun";
+        number_Of_DaysInMonth = 30;
+        break;
+        case 7:
+        MonthOfName = "July";
+        number_Of_DaysInMonth = 31;
+        break;
+        case 8:
+        MonthOfName = "August";
+        number_Of_DaysInMonth = 31;
+        break;
+        case 9:
+        MonthOfName = "September";
+        number_Of_DaysInMonth = 30;
+        break;
+        case 10:
+        MonthOfName = "October";
+        number_Of_DaysInMonth = 31;
+        break;
+        case 11:
+        MonthOfName = "November";
+        number_Of_DaysInMonth = 30;
+        break;
+        case 12:
+        MonthOfName = "December";
+        number_Of_DaysInMonth = 31;
+        break;
+        default:
+        
     }
     System.out.print(MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
   }
